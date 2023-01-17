3fcfec9427c62f6df676ace8af9b8fb1a740f1d0
mllyhrmwt <mellyhermawati@smkwikrama.sch.id>
2023-01-17 14:34:41 +0700

    Service Bengkel

---
+++ home.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Service Bengkel | Home</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	 
+	<title>Bogor Service</title>
+</head>
+<body>
+		<div class="banner">
+			<img src="img/banner10.jpg">
+
+	<footer>
+		<div class="container">
+			<center><small>Copyright © 2021 - Bogor Service</small></center>
+		</div>
+	</footer>
+</body>
+</html>
